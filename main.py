e1eb6485ebdd179fd6d28f61aff8b1c4f218ec1c
hello world <joshuaburge507@gmail.com>
2025-05-30 10:14:01 +0930

    feat: added test surface and title

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Pokemon Battles!")
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((10,10))
+test_surface.fill('Blue')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(test_surface,(200,100))
+
     # Draw all elements
     # Update everything
     pygame.display.update()
+    clock.tick(60)
